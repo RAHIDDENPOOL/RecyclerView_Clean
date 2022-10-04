139fd49e8b4520fc723aca327bca738564e7d7e4
HIDDENPOOL <110696633+RAHIDDENPOOL@users.noreply.github.com>
2022-10-04 19:27:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cleanrecyclerview/Food.kt
@@ -0,0 +1,3 @@
+package com.example.cleanrecyclerview
+
+data class Food(val image: Int, val name: String)
---
+++ app/src/main/java/com/example/cleanrecyclerview/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.cleanrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var foodList: ArrayList<Food>
+    private lateinit var foodAdapter: FoodAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        foodList = ArrayList()
+
+        foodList.add(Food(R.drawable.burger, "Burger"))
+        foodList.add(Food(R.drawable.pasta, "Pasta"))
+        foodList.add(Food(R.drawable.pizza, "Pizza"))
+        foodList.add(Food(R.drawable.sushi, "Sushi"))
+        foodList.add(Food(R.drawable.dinner, "Dinner"))
+        foodList.add(Food(R.drawable.lanch, "Lanch"))
+        foodAdapter = FoodAdapter(foodList)
+        recyclerView.adapter = foodAdapter
+    }
+}
